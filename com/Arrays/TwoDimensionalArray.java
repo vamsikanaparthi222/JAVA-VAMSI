fcb3fc17816ba00c2d9365f7e1569c06450c794b
vamsikanaparthi <kanaparthivamsi222@gmail.com>
2023-03-15 07:54:41 +0530

    chai

--- com/Arrays/TwoDimensionalArray.java
+++ com/Arrays/TwoDimensionalArray.java
@@ -3,8 +3,15 @@ package com.Arrays;
 public class TwoDimensionalArray {
 
 	public static void main(String[] args) {
+		int[][] number = new int[1][2];
+		number[0][1] = 10;
+		number[1][2] = 10;
+		number[2][4] = 10;
+		number[4][6] = 10;
+		number[8][7] = 10;
 		
+		
+		System.out.println(number[1][2]);
 
 	}
-
 }
