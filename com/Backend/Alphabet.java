bc0b9781c131bf4564f0a80c1b83eb3c125d65a9
vamsikanaparthi <kanaparthivamsi222@gmail.com>
2023-04-05 07:25:33 +0530

    chai

--- com/Backend/Alphabet.java
+++ com/Backend/Alphabet.java
@@ -3,10 +3,15 @@ package com.Backend;
 public class Alphabet {
 
 	public static void main(String[] args) {
+		/*
+		 * char ch; for(ch='a';ch<='z';ch++) System.out.println(ch);
+		 */
 		char ch;
-		for(ch='a';ch<='z';ch++)
-		System.out.println(ch);
-
+		for(ch='A';ch<='Z';ch++)
+			System.out.println(ch);
+		int i;
+		for(i=1;i<=100;i++)
+			System.out.println(i);
 	}
 
 }
